27f2f366b0996c160ed264c3e07b33dc3036768e
Southcat Saltmo <3390996860@qq.com>
2025-04-11 23:20:41 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -73,19 +73,11 @@ export default function Home({
         <Text size="xs" mt="xs" mb="xs" style={{
           textAlign: 'center'
         }}>
-          Open-source monitoring and status page powered by{' '}
+          本页面由{' '}
           <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">
             Uptimeflare
           </a>{' '}
-          and{' '}
-          <a href="https://www.cloudflare.com/" target="_blank">
-            Cloudflare
-          </a>
-          , made with ❤ by{' '}
-          <a href="https://github.com/lyc8503" target="_blank">
-            lyc8503
-          </a>
-          .
+          盛情赞助.
         </Text>
       </main>
     </>
